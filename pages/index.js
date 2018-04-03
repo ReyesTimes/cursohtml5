e59441ab0c9fce5eef259e90c67cb4d17b707a8a
Juan Reyes <rated-mvp@hotmail.com>
2018-04-03 18:30:27 -0500

    add first page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Index extends Component {
+    render() {
+        return (
+            <div>Esto es un curso de HTML 5 :D</div>
+        );
+    }
+}
+
+export default Index;
