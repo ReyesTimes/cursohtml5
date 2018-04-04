77092c8136ec8ec2c2f68cbc0aa5bdade82c0d2c
ReyesTimes <rated.mvp1@gmail.com>
2018-04-03 23:52:11 -0500

    add other component and check functionality

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import ExampleComponent from '../containers/Example';
 
 
 class Index extends Component {
     render() {
         return (
-            <div>Esto es un curso de HTML 5 :D</div>
+            <ExampleComponent>
+                :D
+            </ExampleComponent>
         );
     }
 }
